8e2d7e21454e9f5eeb8346645f15a94e3f5e2d21
fannytg <fannytg@gmail.com>
2015-03-28 12:33:46 -0500

    javascript lesson
    javascriot lesson

---
+++ lab1/css/lab2.js
@@ -0,0 +1,17 @@
+/* Array en JavaScript */
+
+//'use strict';
+
+var my_array = new Array();
+
+var other_array = [];
+
+my_array = [1,3,3,3,4,4,555,55,666,6,6,666666];
+
+console.log(my_array);
+
+console.log(my_array[3]);
+
+other_array = ['a', 'n', 'ggjggjjggjgj', 'fjfjfjf'];
+
+console.log(other_array);
