b7b29c3276c974cdf42e547c97c2df5444cea1a9
fannytg <fannytg@gmail.com>
2015-05-08 21:56:02 -0500

    gulp use
    gulp use

---
+++ lab31/source/lab7.js
@@ -0,0 +1,15 @@
+/* Uso de funciones, variables y estructuras de control */
+
+var a = 3;
+var b = 4;
+
+function compara() {
+if(a > b){
+	alert(a + " Es Mayor a " + b);
+} else {
+	alert(a + " Es menor que " + b);
+}
+}
+
+
+compara();
